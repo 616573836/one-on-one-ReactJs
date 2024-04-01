173e0f0aea5d33e9ad6352b066af4ce6de464efd
Sirui Yu <torrestyu2017@gmail.com>
2024-04-01 19:33:44 -0400

    resolve conflicts in App.js

--- one-on-one-fe/src/App.js
+++ one-on-one-fe/src/App.js
@@ -3,11 +3,8 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./components/sign_up";
 import Login from "./components/login";
-<<<<<<< HEAD
 import Profile from "./components/profile";
-=======
 import MeetingList from "./components/meetings";
->>>>>>> b35deff2d9f4ddb389df79d6ea3662f05c0ea217
 
 // Other imports...
 
@@ -17,11 +14,8 @@ function App() {
         <Routes>
         <Route path="signup/" element={<SignUp />} />
         <Route path="login/" element={<Login />} />
-<<<<<<< HEAD
         <Route path="profile/" element={<Profile />} />
-=======
         <Route path="meetings/" element={<MeetingList />} />
->>>>>>> b35deff2d9f4ddb389df79d6ea3662f05c0ea217
 
           {/* Other routes */}
         </Routes>
