61a187317d7b0130bef6b43dd91a45715976cf8b
Xuhui Chen <xuhui.chen@mail.utoronto.ca>
2024-04-09 23:54:34 -0400

    disable accept membership when polling or finalized

--- one-on-one-backend/meetings/views/pending_member_view.py
+++ one-on-one-backend/meetings/views/pending_member_view.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from ..models.calendar import Calendar
 from rest_framework.permissions import AllowAny
+from ..models.meeting import Meeting
 
 from .meeting_views import update_meeting_state
 
@@ -16,6 +17,11 @@ from .meeting_views import update_meeting_state
 def confirm_member(request, token):
     pending_member = PendingMember.objects.filter(token=token).first()
     if pending_member:
+        meeting = Meeting.objects.get(pk=pending_member.meeting_id)
+
+        if meeting.state == "approving" or meeting.state == "finalized":
+            pending_member.delete()
+            return Response({"error": "You cannot join the meeting."}, status=status.HTTP_400_BAD_REQUEST)
         Member.objects.create(user_id=pending_member.user_id, meeting_id=pending_member.meeting_id)
         Calendar.objects.create(meeting_id=pending_member.meeting_id, owner_id=pending_member.user_id)
         update_meeting_state(pending_member.meeting_id)
